68fa8f5516cce944f8c768d661068db6ddd03414
robot-ci-heartex <87703623+robot-ci-heartex@users.noreply.github.com>
2024-11-15 17:04:08 +0000

    feat: DIA-1577: OpenAI usage limits for Trial accounts (#6644)
    Co-authored-by: fern-api <115122769+fern-api[bot]@users.noreply.github.com>
    Co-authored-by: nik <nik@heartex.net>
    Co-authored-by: niklub <niklub@users.noreply.github.com>

--- label_studio/ml_model_providers/models.py
+++ label_studio/ml_model_providers/models.py
@@ -1,8 +1,11 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
+from typing import List
+
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from tasks.models import PredictionMeta
 
 
 class ModelProviders(models.TextChoices):
@@ -54,6 +57,59 @@ class ModelProviderConnection(models.Model):
 
     updated_at = models.DateTimeField(_('updated at'), auto_now=True)
 
+    is_internal = models.BooleanField(
+        _('is_internal'),
+        default=False,
+        help_text='Whether the model provider connection is internal, not visible to the user',
+        null=True,
+        blank=True,
+    )
+
+    budget_limit = models.FloatField(
+        _('budget_limit'),
+        null=True,
+        blank=True,
+        default=None,
+        help_text='Budget limit for the model provider connection (null if unlimited)',
+    )
+
+    budget_last_reset_date = models.DateTimeField(
+        _('budget_last_reset_date'),
+        null=True,
+        blank=True,
+        default=None,
+        help_text='Date and time the budget was last reset',
+    )
+
+    budget_reset_period = models.CharField(
+        _('budget_reset_period'),
+        max_length=20,
+        choices=[
+            ('Monthly', 'Monthly'),
+            ('Yearly', 'Yearly'),
+        ],
+        null=True,
+        blank=True,
+        default=None,
+        help_text='Budget reset period for the model provider connection (null if not reset)',
+    )
+
+    budget_total_spent = models.FloatField(
+        _('budget_total_spent'),
+        null=True,
+        blank=True,
+        default=None,
+        help_text='Tracked total budget spent for the given provider connection within the current budget period',
+    )
+
+    budget_alert_threshold = models.FloatField(
+        _('budget_alert_threshold'),
+        null=True,
+        blank=True,
+        default=None,
+        help_text='Budget alert threshold for the given provider connection',
+    )
+
     # Check if user is Admin or Owner
     # This will need to be updated if we ever use this model in LSO as `is_owner` and
     # `is_administrator` only exist in LSE
@@ -61,3 +117,8 @@ class ModelProviderConnection(models.Model):
         return (
             user.is_administrator or user.is_owner or user.is_manager
         ) and user.active_organization_id == self.organization_id
+
+    def update_budget_total_spent_from_predictions_meta(self, predictions_meta: List[PredictionMeta]):
+        total_cost = sum(meta.total_cost or 0 for meta in predictions_meta)
+        self.budget_total_spent = total_cost
+        self.save(update_fields=['budget_total_spent'])
