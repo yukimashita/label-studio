65d1de4a06a8cf4868f053c963be03f5638d2ed5
Hakan Erol <hakan@humansignal.com>
2024-11-20 15:23:27 -0800

    fix: DIA-1655: SC Prompter budget getting reset to 0 (#6692)
    Co-authored-by: hakan458 <hakan@heartex.com>

--- label_studio/ml_model_providers/models.py
+++ label_studio/ml_model_providers/models.py
@@ -120,5 +120,5 @@ class ModelProviderConnection(models.Model):
 
     def update_budget_total_spent_from_predictions_meta(self, predictions_meta: List[PredictionMeta]):
         total_cost = sum(meta.total_cost or 0 for meta in predictions_meta)
-        self.budget_total_spent = total_cost
+        self.budget_total_spent += total_cost
         self.save(update_fields=['budget_total_spent'])
